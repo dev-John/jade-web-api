27a69935c172e7be405fb00ade890e5948f69815
Jonatas <jonatasoliveira1234@gmail.com>
2020-12-19 23:32:37 -0300

    wip

--- src/routes/person.js
+++ src/routes/person.js
@@ -23,4 +23,61 @@ export default [
       }
     },
   },
+
+  {
+    method: "GET",
+    path: "/get-people",
+
+    async handler(req, h) {
+      try {
+        const { data } = await api.get("/get-people");
+
+        return h.response({ status: RESPONSE_STATUS.SUCCESS, data });
+      } catch (error) {
+        return h
+          .response({ status: RESPONSE_STATUS.FAIL, message: error.message })
+          .code(HTTP_CODES.FAIL_VALIDATION);
+      }
+    },
+  },
+
+  {
+    method: "GET",
+    path: "/search-person",
+
+    async handler(req, h) {
+      const { cpfCnpj, uf, city } = req.query;
+
+      try {
+        const { data } = await api.get(
+          `/search-person/${cpfCnpj}/${uf}/${city}`
+        );
+
+        return h.response({ status: RESPONSE_STATUS.SUCCESS, data });
+      } catch (error) {
+        return h
+          .response({ status: RESPONSE_STATUS.FAIL, message: error.message })
+          .code(HTTP_CODES.FAIL_VALIDATION);
+      }
+    },
+  },
+
+  {
+    method: "DELETE",
+    path: "/delete-person/{_id}",
+
+    async handler(req, h) {
+      const { _id } = req.params;
+
+      try {
+        await api.delete(`/delete-person/${_id}`);
+
+        return h.response({ status: RESPONSE_STATUS.SUCCESS });
+      } catch (error) {
+        return h
+          .response({ status: RESPONSE_STATUS.FAIL, message: error.message })
+          .code(HTTP_CODES.FAIL_VALIDATION);
+      }
+    },
+  },
 ];
